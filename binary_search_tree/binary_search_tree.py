899d4eeedcc48d52abcdade3b22f683c6d5ce71c
Miley Wright <dishaliekwright@yahoo.com>
2020-07-28 19:25:32 -0400

    get max

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -53,7 +53,10 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right is None: # if the right side is None
+            return self.value # then that is the highest value
+        else: # otherwise keep going
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
